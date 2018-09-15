37b56df05ce4a85091c5ad3990e79796bb1ea817
jashment <jordanashment@gmail.com>
2018-09-14 21:19:37 -0600

    Trying to figure out Vue App

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,16 @@
 <template>
-<h1>Hello World!</h1>
+<html>
+    <body>
+        <div>
+            <h1>Hello World!</h1>
+            <h2>Everything fits in the template?</h2>
+            <div>
+                <p>Does the CSS work?</p>
+                <h3>Is this really how the page is built?</h3>
+            </div>
+        </div>
+    </body>
+</html>
 </template>
 
 <script>
@@ -9,5 +20,12 @@ export default {
 </script>
 
 <style>
+    div {
+        text-align: center;
+    }
 
+    div div p {
+        color: blue;
+        background-color: gainsboro;
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,5 +3,9 @@ import App from './App.vue'
 
 new Vue({
   el: '#app',
+  data: {
+    name: 'Jordan Ashment',
+    age: 27
+  },
   render: h => h(App)
 })
