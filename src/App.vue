fd4a0fa3c4e3ae8c3cbda4fdfb26a33c13de663e
jashment <jordanashment@gmail.com>
2018-09-23 19:03:30 -0600

    Multiple Changes Practice

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,7 @@
 <template>
-<html>
-    <body>
-        <div>
-            <h1>Hello World!</h1>
-            <h2>Everything fits in the template?</h2>
-            <div>
-                <p>Does the CSS work?</p>
-                <h3>Is this really how the page is built?</h3>
-            </div>
-        </div>
-    </body>
-</html>
+<app-server-status></app-server-status>
 </template>
 
-<script>
-</script>
 
 <style>
-    div {
-        text-align: center;
-    }
-
-    div div p {
-        color: blue;
-        background-color: gainsboro;
-    }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
+import Vuetify from 'vuetify'
 import App from './App.vue'
+import Home from './Home.vue'
+
+Vue.compontent ('app-server=status', Home)
 
 new Vue({
   el: '#app',
